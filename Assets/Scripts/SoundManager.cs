24c4549268ff5b939a0a1c2694b4daf4c252f3e8
Carlton Mpofu <mpofucarlton0@gmail.com>
2024-02-22 08:33:28 +0200

    Fix: Sound Manager gameobject had two scripts

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -19,11 +19,6 @@ public class SoundManager : MonoBehaviour
         soundOn = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void PlaySound()
     {
